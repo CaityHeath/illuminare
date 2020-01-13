ecb77ec67fa6fafb590ff3b40ceccd47e7515f3e
CaityHeath <caity.heath@gmail.com>
2020-01-13 15:35:42 -0800

    fetching data from backend api server

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    google_civic: 'AIzaSyAAoE7GA9s53_NrusNCv0RVW_h2hZxW-FY',
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,74 @@
 import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
+import superagent from 'superagent'
+import React, {useState} from 'react';
 
-let google_civic = process.env.google_civic;
 
-const Index = () => (
+
+
+function Index(){
+  const [address, setAddress] = useState();
+  const [city, setCity] = useState();
+  const [state, setState] = useState();
+  const [query, setQuery] = useState();
+
+  const handleChange= (e) => {  
+    e.preventDefault();
+    setAddress(e.target.value);
+  }
+
+  const handleCityChange = (e) => {
+    e.preventDefault();
+    setCity(e.target.value);
+  }
+
+  const handleStateChange = (e) => {
+    e.preventDefault();
+    setState(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    normalizeRequest(address, city, state);
+  }
+
+
+  const createQueryString = (input) => {
+    let seperator = input.split(" ");
+    let query = seperator.join('%20');
+    return query;
+  }
+
+   const fetchData = async (query) => {
+    console.log(query);
+
+    const fetch = await superagent.get('http://localhost:9000/elections').query({query: query});
+    console.log(fetch.body);
+  }
+
+  const normalizeRequest = (address, city, state) => {  
+    let add = createQueryString(address);
+    let cs = createQueryString(city);
+    let st = createQueryString(state);
+    let query = `${add}%20${cs}%20${st}`;
+    return fetchData(query);
+  }
+
+  return (
   <div>
     <Link href="/about">
         <a  title="About" >About Page</a>
     </Link>
-    <p>Hello Next.js</p>
+    <form onSubmit={(e) => handleSubmit(e)} >
+      <input type="text" placeholder="Street address" value={address} onChange={(e) => handleChange(e)}/>
+      <input type="text" placeholder="City" value={city} onChange={(e) => handleCityChange(e)}/>
+      <input type="text" placeholder="State" value={state} onChange={(e) => handleStateChange(e)}/>
+      <button type="submit">Submit</button>
+    </form>
+  <p> { address } </p>
+  <p> {city} </p>
+  <p> {state} </p>
 
   </div>
-);
-
-
-Index.getInitialProps = async function() {
-  const res = await fetch(`https://googleapis.com/civicinfo/v2/elections?key=${google_civic}`);
-  const data = await res.text();
-
-  console.log(`data fetched ${data.length}`)
-  return {data};
-}
+  );
+};
 
 export default Index;
